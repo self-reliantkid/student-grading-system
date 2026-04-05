dc6b03e1e25468f125396dc324996dc6827ec19a
self-reliantkid <syfolikumah@gmail.com>
2026-04-05 18:04:58 +0000

    main menu completed

---
+++ README.md
@@ -0,0 +1,4 @@
+## Project Details
+- This is an app made to allow university students interpret their semester results accurately. 
+- Terminal app
+- Students can choose between two grading systems (CGPA or CWA)
---
+++ student_grading_system.py
@@ -0,0 +1,54 @@
+#Preferred grading system
+import time
+
+
+
+#main menu
+def main_menu():
+    print("Student Grading System")
+    print("Select an option from the menu below:")
+    print(" 1. Interpret results")
+    print(" 2. Quit")
+
+    try:
+        user_choice = int(input("\nYour choice: "))
+        if user_choice in range(1, 3):
+            if user_choice == 1:
+                running_menu()
+            else:
+                quit_app()    
+        else:
+            print("Option not in menu. Try again!\n\n")
+            time.sleep(0.8)
+            main_menu
+    except ValueError:
+        print("Invalid input! Kindly select an appropriate option\n\n")
+        time.sleep(0.8)
+        main_menu()
+
+
+
+def running_menu():
+    pass
+
+
+
+def calc_gpa():
+    pass
+
+
+
+#return menu
+def return_menu():
+    pass
+
+
+
+#quit
+def quit_app():
+    pass
+
+
+
+if __name__ == "__main__":
+    main_menu()
